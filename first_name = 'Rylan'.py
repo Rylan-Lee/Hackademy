2fa7133b9472f07f95c22dba4cbd7c146ac70de8
Rylan Lee <rylanlee@Rylans-Air.lan>
2025-06-05 18:00:45 -0400

    Added google embedding

---
+++ first_name = 'Rylan'.py
@@ -0,0 +1,14 @@
+first_name = 'Rylan '
+last_name = 'Lee '
+middle_name ='Aiden '
+
+full_name = first_name + middle_name + last_name
+print(full_name)
+
+# recipe
+
+num_eggs = 6
+cups_milk = 2
+cups_flour = 2
+
+print("the recipe requires" num_eggs)
